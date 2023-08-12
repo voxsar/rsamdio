9df1e3c2d065ec18953f4587fb7ca2aee59dfa2e
Miyuru Dharmage <miyuru@artslabcreatives.com>
2023-08-12 18:12:37 +0530

    m

--- resources/views/reports/create.blade.php
+++ resources/views/reports/create.blade.php
@@ -6,7 +6,7 @@
 			<h5 class="card-title"></h5>
 		</div>
 		<div class="card-body">
-			<p class="card-description">Here’s a quick example to demonstrate simple input field. Add <code>.light-focus</code> to <code>.form-control</code> to get lighter border on focus.</p>
+			<p class="card-description"></p>
 			<div class="example-container">
 				<div class="example-content">
 					<label for="exampleInputEmail1" class="form-label">Total Rotaract Clubs in the District</label>
